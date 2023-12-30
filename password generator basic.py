e68c62891b6d4f19243fe0ba41845b63106aedf0
JoseCalil <97139929+JoseCalil@users.noreply.github.com>
2023-12-30 00:30:56 -0500

    Update password generator.py
    I made it a bit more complex, you can now have a requirement of 1 capital letter, 1 lower case, and 1 special character.

--- password generator basic.py
+++ password generator basic.py
@@ -1,15 +1,29 @@
 import random
+import string
 
 def password_generator():
-    chars  = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*'
-    password =''
 
-    for x in range(5):
-        password += random.choice(chars)
+    length = int(input('How many characters do you need? \n'))
+    
+    if length < 3:
+        print("\nPassword length must be at least 3 characters to meet the requirements!\n")
+        print("Please select a valid length. \n")
+        print("-----------------------------------------------------------------------")
+        password_generator()
+    
+    uppercase_char = random.choice(string.ascii_lowercase)
+    lowercase_char = random.choice(string.ascii_uppercase)
+    special_char = random.choice('!@#$%^&*')
+
+    rest_of_password = length -3
+    all_chars = string.ascii_letters + '1234567890!@#$%^&*'
+    random_chars = ''.join(random.choice(all_chars) for _ in range(rest_of_password))
+
+    password = uppercase_char + lowercase_char + special_char +random_chars
     return password
 
 random_password = password_generator()
-print(f"Hello, your random password is: {random_password}" )
+print(f"Your new random password is: {random_password}" )
 
 
 
