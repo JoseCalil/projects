526e61fe41bc16ff06913e13ee14811606f92a01
JoseCalil <97139929+JoseCalil@users.noreply.github.com>
2024-01-02 00:40:40 -0500

    Update To Do List.py
    Big Functional Updates Completed
    -Added a completed task list and the ability to complete your tasks. This will transfer them from your task_list to your completed_task_list.
    -You can now view both your active or completed tasks under the view tasks option.
    
    Small Updates Completed
    -Ability to add multiple tasks without having to go to main menu again
    -Ability to delete multiple tasks without having to go to main menu again
    -Added a few print lines that will space out the text prompts making it easier to read.
    
    Future Updates:
    -Learn to it store all your tasks into an excel spreadsheet or to learn to make a GUI for this for a nicer UI.
    
    I think this project is just about done, the excel sheet would be the final update as this would allow all your inputs to be stored on the local end and not lost every time the program is exited.

--- To Do List.py
+++ To Do List.py
@@ -7,18 +7,28 @@ Create a to-do list, it should have the following features:
 
 """
 task_list = []
+completed_task_list = []
 
 
 class task_options: 
-    #Next feature is to add the ability to add another task
+#Allows you to add tasks to your task_list
     def adding_tasks(self):
-        add_task = input("What task would you like to add? ")
-        task_list.append(add_task)
-        print(f"Awesome! {add_task} has been added to your To-Do list!")
-        print("-----------------------------------------------------------------------")
-        main_menu()
+        while True:
+            add_task = input("What task would you like to add? Type 'menu' to return to the main menu. \n")
+            
+
+            if add_task.lower().strip() == 'menu':
+                print("-----------------------------------------------------------------------")
+                main_menu()
+            else:
+                task_list.append(add_task)
+                print("-----------------------------------------------------------------------")
+                print(f"Awesome! {add_task} has been added to your To-Do list!")
+                print("-----------------------------------------------------------------------")
+                self.adding_tasks()
                 
-    #Next feature is to add the ability to delete another task
+#Allows you to delete tasks from your task_list
+#Will add feature eventually to delete from your Completed Tasks List if user wants
     def deleting_tasks(self):
         while True:   
             print(f"Here are your current tasks: ")
@@ -26,7 +36,8 @@ class task_options:
                 print(f"\n{index}.{task}")
             print("\nType 'menu' to return back to the main menu")
 
-            delete_choice = input("\nWhich task would you like to delete? ")
+            delete_choice = input("\nWhich task number would you like to delete? ")
+            
 
             if delete_choice.lower() == 'menu':
                 print("-----------------------------------------------------------------------")
@@ -38,27 +49,74 @@ class task_options:
                 print("-----------------------------------------------------------------------")
                 print(f"Task {deleted_task} has been deleted successfully!")
                 print("-----------------------------------------------------------------------")
+                self.deleting_tasks()
 
             except(ValueError, IndexError) :
                 print("\nPlease select a proper number or 'menu' to return to the main menu: \n")
 
-    #Next feature to be added, this will probably be a boolean method to transfer tasks that are completed
-    #From the task_list [] to completed_task_list[]
-    #I'll need to look into how to transfer items from 1 list to another
+#Allows you to transfer current tasks from your task_list to completed_task_list
     def completing_tasks(self):
-#       complete_task = input("What task would you like to complete? ")
-        pass
+        while True:   
+            print(f"Here are your current tasks: ")
+            for index, task in enumerate(task_list, start = 1):
+                print(f"\n{index}.{task}")
+            print("\nType 'menu' to return back to the main menu")
 
-    #itemizes all tasks inputted
+            complete_task = input("What task number would you like to complete? ")
+            
+
+            if complete_task.lower() == 'menu':
+                print("-----------------------------------------------------------------------")
+                main_menu()
+
+            try :
+                complete_task =int(complete_task)
+                complete_task = task_list.pop(complete_task - 1)
+                completed_task_list.append(complete_task)
+                print("-----------------------------------------------------------------------")
+                print(f"Task {complete_task} has been completed successfully!")
+                print("-----------------------------------------------------------------------")
+
+            except(ValueError, IndexError) :
+                print("\nPlease select a proper number or 'menu' to return to the main menu: \n")
+
+
+#Allows you to choose to view your current tasks and your completed tasks
     def viewing_tasks(self):
-        print("Here are your current tasks: ", task_list)
-        for index, task in enumerate(task_list, start = 1):
-            print(f" {index}.{task}")
+        print("\nWhich tasks would you like to see? Type 'menu' to return to the main menu.")
+        print("\n1. Current Active Tasks")
+        print("\n2. Completed Task List")
+        task_list_options = input("\nEnter Decision Here: ")
+        print("-----------------------------------------------------------------------")
+
+        if task_list_options == '1':
+            print("-----------------------------------------------------------------------")
+            print("Here are your current tasks: ")
+            for index, task in enumerate(task_list, start=1):
+                print(f" {index}.{task}")
+                self.viewing_tasks()
+
+        elif task_list_options == '2':
+            print("Here are your completed tasks: ")
+            for index, task in enumerate(completed_task_list, start=1):
+                print(f" {index}.{task}")
+                print("-----------------------------------------------------------------------")
+                self.viewing_tasks()
+
+        elif task_list_options.lower().strip() =='menu':
+            print("-----------------------------------------------------------------------")
+            main_menu()
+
+        else:
+            print("-----------------------------------------------------------------------")
+            print("\nPlease select a proper option.\n")
+            print("-----------------------------------------------------------------------")
+            self.viewing_tasks()
+
 
         
 
-#contains all the options available, still working on completing tasks option.
-#future feature will be to add and delete multiple tasks without going back to the main menu everytime. 
+#contains all the options available
 def main_menu():
     
     print("How can I help you today?")
@@ -72,8 +130,9 @@ def main_menu():
         print("\n3. Complete a task on your list")
         print("\n4. View tasks in your list")
         print("\n5. I\'m all done for now")
-
+        print("-----------------------------------------------------------------------")
         choice = input("\nEnter your choice here:  ")
+        
 
         if choice == '1':
             print("-----------------------------------------------------------------------")
@@ -85,8 +144,8 @@ def main_menu():
             break
         if choice == '3':
             print("-----------------------------------------------------------------------")
-            
-            pass
+            options.completing_tasks()
+            break
         if choice == '4':
             print("-----------------------------------------------------------------------")
             options.viewing_tasks()
